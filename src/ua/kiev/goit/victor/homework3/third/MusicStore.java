a49bb4d287bb52bb56485c865d8daf8a21f05842
Victor <lucky.m.victor@gmail.com>
2016-03-14 17:44:02 +0200

    homework

---
+++ src/ua/kiev/goit/victor/homework3/third/MusicStore.java
@@ -0,0 +1,10 @@
+package ua.kiev.goit.victor.homework3.third;
+
+import java.util.List;
+
+/**
+ * Created by Victor on 14.03.2016.
+ */
+public class MusicStore {
+    List<MusicalInstrument> musicalInstruments;
+}
---
+++ src/ua/kiev/goit/victor/homework3/third/MusicalInstrument.java
@@ -0,0 +1,7 @@
+package ua.kiev.goit.victor.homework3.third;
+
+/**
+ * Created by Виктор on 14.03.2016.
+ */
+public class MusicalInstrument {
+}
